8e74884f560f61b4087437a4832b4dbb5542be47
Pasha <ap550697@gmail.com>
2023-11-10 22:48:58 +0330

    multiply func done.

--- SparseMatrix/SparseMatrix.py
+++ SparseMatrix/SparseMatrix.py
@@ -26,13 +26,17 @@ class SparseMatrix:
         sorted_transposed = [transposed_matrix[0]]
         transposed_matrix.pop(0)
 
-        sorted_transposed += sorted(transposed_matrix, key=lambda x: x[0])
+        sorted_transposed += sorted(transposed_matrix, key=lambda x: (x[0], x[1]))
 
         return sorted_transposed
 
     def add(self, other_matrix):
+
+        copyOfSelf = self.sparse_matrix_list.copy()
+        copyofOther = other_matrix.sparse_matrix_list.copy()
+
         if (self.rowcount != other_matrix.rowcount or self.columncount != other_matrix.columncount):
-            raise Exception("can't be added")
+            raise Exception("Matrix dimensions mismatch. The matrices cannot be added.")
 
         else:
             valuecountUnited = 0
@@ -57,9 +61,33 @@ class SparseMatrix:
 
             united_matrix[0][2] = valuecountUnited
             sorted_united = [united_matrix[0]]
-            sorted_united += sorted(united_matrix[1:], key=lambda x: x[0])
+            sorted_united += sorted(united_matrix[1:], key=lambda x: (x[0], x[1]))
+
+            for x in sorted_united:
+                if (x[2] == 0):
+                    sorted_united.remove(x)
+                    valuecountUnited -= 1
+                    sorted_united[0][2] = valuecountUnited
+
+            self.sparse_matrix_list = copyOfSelf
+            other_matrix.sparse_matrix_list = copyofOther
 
             return sorted_united
 
-    def multiply(self):
-        pass
+    def multiply(self, other_matrix):
+        if self.columncount != other_matrix.rowcount:
+            raise Exception(
+                "The number of columns in the first matrix must be equal to the number of rows in the second matrix for multiplication.")
+
+        result_matrix = SparseMatrix(self.rowcount, other_matrix.columncount)
+
+        for i in range(1, len(self.sparse_matrix_list)):
+            for j in range(1, len(other_matrix.sparse_matrix_list)):
+                if self.sparse_matrix_list[i][1] == other_matrix.sparse_matrix_list[j][0]:
+                    result_matrix.insert(self.sparse_matrix_list[i][0], other_matrix.sparse_matrix_list[j][1],
+                                         self.sparse_matrix_list[i][2] * other_matrix.sparse_matrix_list[j][2])
+
+        sorted_result = [result_matrix.sparse_matrix_list[0]]
+        result_matrix.sparse_matrix_list.pop(0)
+        sorted_result += sorted(result_matrix.sparse_matrix_list, key=lambda x: (x[0], x[1]))
+        return sorted_result
--- SparseMatrix/main.py
+++ SparseMatrix/main.py
@@ -14,25 +14,35 @@ def print_custom(input_matrix):
 
 
 def main():
-    matrix1 = SparseMatrix(4, 5)
-    matrix1.insert(0, 4, 8)
-    matrix1.insert(1, 1, 9)
-    matrix1.insert(2, 3, 3)
+    print("The First Sparse Matrix:")
+    matrix1 = SparseMatrix(4, 4)
+    matrix1.insert(1, 2, 10)
+    matrix1.insert(1, 4, 12)
+    matrix1.insert(3, 3, 5)
+    matrix1.insert(4, 1, 15)
+    matrix1.insert(4, 2, 12)
     print_custom_object(matrix1)
 
-    print()
-
+    print("\nTranspose Of First Sparse Matrix:")
     transposed_matrix = matrix1.transpose()
     print_custom(transposed_matrix)
 
-    print()
+    print("\nThe Second Sparse Matrix:")
+    matrix2 = SparseMatrix(4, 4)
+    matrix2.insert(1, 3, 8)
+    matrix2.insert(2, 4, 23)
+    matrix2.insert(3, 3, 9)
+    matrix2.insert(4, 1, 20)
+    matrix2.insert(4, 2, 25)
+    print_custom_object(matrix2)
 
-    matrix2 = SparseMatrix(4, 5)
-    matrix2.insert(3, 0, 8)
-    matrix2.insert(1, 2, 9)
-    matrix2.insert(2, 3, 3)
-    output = matrix1.add(matrix2)
-    print_custom(output)
+    print("\nAddition OF First And Second Sparse Matrix:")
+    addition_output = matrix1.add(matrix2)
+    print_custom(addition_output)
+
+    print("\nMultiplication Of First And Second Sparse Matrix:")
+    multiplication_output = matrix1.multiply(matrix2)
+    print_custom(multiplication_output)
 
 
 if __name__ == '__main__':
